9f7df7d8144c2b7477f8e8757c36acb58b9c1442
xianren <2958197921@qq.com>
2023-04-24 17:22:35 +0800

    修改原地算法

--- 04_复合数据类型/4.5.go
+++ 04_复合数据类型/4.5.go
@@ -5,18 +5,19 @@ import "fmt"
 
 func main() {
 	str := []string{"11", "11", "22", "23", "24", "24", "37"}
-	deduplication(str)
-	fmt.Println(str) // [11 22 23 24 37  ]
+	str = deduplication(str)
+	fmt.Println(str) // [11 22 23 24 37]
 }
 
 // 原地消除重复值函数
-func deduplication(str []string) {
+func deduplication(str []string) []string {
 	for i := 1; i < len(str); {
 		if str[i] == str[i-1] {
 			copy(str[i:], str[i+1:])
-			// 最后一位设为空串
-			str[len(str)-1] = ""
+			// 删除最后一位
+			str = str[:len(str)-1]
 		}
 		i++
 	}
+	return str
 }
